c3a3d29c9ac7f76330025cb750ed0b6108f63911
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-07-06 00:47:45 +0300

    done changes to my object

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 
 // I've invited you to fill out a form:
 // Objects
-// Given an array of objects, each object representing a person with a name and age property, write a function that returns a new array containing the names of all people who are 18 years old or older.
+// Given an array of objects, each object representing a person with a name and age property, 
+// write a function that returns a new array containing the names of all people who are 18 years
+//  old or older.
 // const people = [
 //   { name: 'Alice', age: 17 },
 //   { name: 'Eunice', age: 22 },
@@ -25,11 +27,11 @@ const people = [
       }
       return teen
   }
+  console.log(person(people));
 
-  const x = person(people)
-  console.log(x);
-
-//   Given an array of objects, where each object represents a student with a name and an array of scores, write a function that returns a new array containing the names of all students whose average score is greater than or equal to 85.
+//   Given an array of objects, where each object represents a student with a name and an array 
+// of scores, write a function that returns a new array containing the names of all students whose
+//  average score is greater than or equal to 85.
 //   const students = [
 //     { name: 'John', scores: [90, 80, 85] },
 //     { name: 'Jane', scores: [95, 92, 88] },
@@ -56,9 +58,15 @@ const people = [
       return highArr
     })
   }
+  const scoringStudents = highestScore(stud)
+  console.log(scoringStudents)
+  const products = [
 
 
-//   Write a function that takes an array of objects, where each object represents a product with a name, price, and category property. The function should return an object that groups the products by their categories, with the category names as keys and the arrays of products as values.
+//   Write a function that takes an array of objects, where each object represents a product with
+//  a name, price, and category property. The function should return an object that groups the 
+//  products by their categories, with the category names as keys and the arrays of products as
+//   values.
 // const products = [
 //   { name: 'Laptop', price: 1200, category: 'Electronics' },
 //   { name: 'Shirt', price: 25, category: 'Clothing' },
@@ -66,9 +74,9 @@ const people = [
 //   { name: 'Shoes', price: 60, category: 'Clothing' },
 // ];
 
-  const scoringStudents = highestScore(stud)
-  console.log(scoringStudents)
-  const products = [
+  // const scoringStudents = highestScore(stud)
+  // console.log(scoringStudents)
+  // const products = [
     { name: 'Laptop', price: 1200, category: 'Electronics' },
     { name: 'Shirt', price: 25, category: 'Clothing' },
     { name: 'Headphones', price: 80, category: 'Electronics' },
@@ -92,7 +100,10 @@ const people = [
   console.log(separate(products));
 
 
-//   Given an object representing a car, with properties for the make, model, year, and a method to display the car's information, write a function that takes the car object and adds a new method to the object called age. The age method should return the current age of the car based on the current year and the car's year property.
+//   Given an object representing a car, with properties for the make, model, year, and a method 
+// to display the car's information, write a function that takes the car object and adds a new method 
+// to the object called age. The age method should return the current age of the car based on the 
+// current year and the car's year property.
 //   const car = {
 //     make: 'Ford',
 //     model: 'Ranger',
